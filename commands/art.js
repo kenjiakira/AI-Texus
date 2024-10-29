9aa0537e170036cc5f47c5af18b69189995c8c56
Akira <kenjiakira2006@gmail.com>
2024-10-29 22:50:05 +0700

    Add

---
+++ commands/art.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  
+  name: 'art',  
+  description: 'Tạo ảnh AI từ Prompt của bạn', 
+  usage: '/generateImage [prompt]', 
+  author: 'MakoyQx',  
+
+  async execute(senderId, args, pageAccessToken) {
+   
+    if (!args || args.length === 0) {
+    
+      await sendMessage(senderId, {
+        text: '❌ Vui lòng nhập Prompt\n\nVí dụ: Art Chó , Mèo V..v.'
+      }, pageAccessToken);
+      return;  
+    }
+
+    const prompt = args.join(' ');
+    const apiUrl = `https://joshweb.click/api/art?prompt=${encodeURIComponent(prompt)}`;  
+
+    await sendMessage(senderId, { text: '⌛ Đang tạo ảnh , Đợi chút....' }, pageAccessToken);
+
+    try {
+      
+      await sendMessage(senderId, {
+        attachment: {
+          type: 'image',
+          payload: {
+            url: apiUrl  
+          }
+        }
+      }, pageAccessToken);
+
+    } catch (error) {
+      // Handle and log any errors during image generation
+      console.error('Error generating image:', error);
+      
+      // Notify user of the error
+      await sendMessage(senderId, {
+        text: 'Có lỗi gì đó đã xảy ra vui lòng thử lại sau nha.'
+      }, pageAccessToken);
+    }
+  }
+};
